0f84a0bc723436b4e8a32fd28f9c1bbe5807c3cd
Maria <wolfmari57@gmail.com>
2023-07-16 22:16:03 -0600

    Ajuste de funciones-2

--- Listacontactos.js
+++ Listacontactos.js
@@ -1,24 +1,49 @@
-let contactos = ["Maria Lobo", "Miguel Lopez", "Bernardo Sosa", "Marisol Jimenez"];
+let contactos = [];
 
 function agregarContacto(contacto) {
   contactos.push(contacto);
   console.log("Contacto añadido:", contacto);
 }
 
-function borrarContacto(contacto) {
-  let indice = listaContactos.indexOf(contacto);
+function borrarContacto(id) {
+  let indice = -1;
+  for (let i = 0; i < contactos.length; i++) {
+    if (contactos[i].id === id) {
+      indice = i;
+      break;
+    }
+  }
+  
   if (indice !== -1) {
-    listaContactos.splice(indice, 1);
-    console.log("Contacto borrado:", contacto);
+    let contactoBorrado = contactos.splice(indice, 1);
+    console.log(
+      "Contacto borrado:",
+      contactoBorrado[0].nombres,
+      contactoBorrado[0].apellidos
+    );
   } else {
     console.log("El contacto no existe en la lista.");
   }
 }
 
 function imprimirContactos() {
-  console.log("Lista de contactos:");
-  for (let contacto of listaContactos) {
-    console.log(contacto);
+  console.log("contactos:");
+  for (let contacto of contactos) {
+    console.log("ID:", contacto.id);
+    console.log("Nombre completo:", contacto.nombres, contacto.apellidos);
+    console.log("Teléfono:", contacto.telefono);
+    console.log("Ciudad:", contacto.ciudad);
+    console.log("Dirección:", contacto.direccion);
   }
 }
-  
+
+let nuevoContacto = {
+  id: 0,
+  nombres: "Maria Jose",
+  apellidos: "Lobo",
+  telefono: "8732-2133",
+  ciudad: "Villanueva",
+  direccion: "Kilometro 71",
+};
+
+agregarContacto(nuevoContacto);
